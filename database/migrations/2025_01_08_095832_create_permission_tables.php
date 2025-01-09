abae463d00f178ca5d9d67f02c0732a738201a98
Lourdes <lourdeswairimu@gmail.com>
2025-01-09 20:01:00 +0300

    landing page

--- database/migrations/2025_01_08_095832_create_permission_tables.php
+++ database/migrations/2025_01_08_095832_create_permission_tables.php
@@ -27,11 +27,11 @@ return new class extends Migration
         Schema::create($tableNames['permissions'], function (Blueprint $table) {
             //$table->engine('InnoDB');
             $table->bigIncrements('id'); // permission id
-            $table->string('name');       // For MyISAM use string('name', 225); // (or 166 for InnoDB with Redundant/Compact row format)
-            $table->string('guard_name'); // For MyISAM use string('guard_name', 25);
+            $table->string('name', 191);       // For MyISAM use string('name', 225); // (or 166 for InnoDB with Redundant/Compact row format)
+            $table->string('guard_name', 191); // For MyISAM use string('guard_name', 25);
             $table->timestamps();
 
-            $table->unique(['name', 'guard_name']);
+            $table->unique(['name(191)', 'guard_name(191)'], 'permissions_name_guard_name_unique');
         });
 
         Schema::create($tableNames['roles'], function (Blueprint $table) use ($teams, $columnNames) {
